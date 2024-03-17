21605c847814ef9c111a69289f1394e721665eef
exploralgo <admin@exploralgo.fr>
2024-03-17 18:28:12 +0100

    ajout php

--- wp-content/themes/motaphoto/single-photo.php
+++ wp-content/themes/motaphoto/single-photo.php
@@ -1,69 +1,40 @@
 <?php 
 get_header(); ?>
- 
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
 
-            <div class="content-wrapper">
-                
-                <div class="content">
-                    <div class="content-info">
-                        <h1 class="title"></h1>
-                        <p class="property"></p>
-                        <p class="property"></p>
-                        <p class="property"></p>
-                        <p class="property"></p>
-                        <p class="property"></p>
-                    </div>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
 
-                    <!-- div ou directement image? -->
-                    <div class="content-photo">
-                        <img src="" alt="">
-                    </div>
-                </div>
-                
-                <div class="interact">
-                    <div class="interact-contact">
-                        <p class="text"></p>
-                        <button class="grey-btn"></button>
-                    </div>
-
-                    <div class="interact-nav">
-                        <div class="previous-wrapper">
-                            <div class="arrow"></div>
-                            <img class="arrow-img"src="" alt="">
-                        <div class="next-wrapper">
-                            <div class="arrow"></div>
-                            <img class="arrow-img"src="" alt="">
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
- 
         <?php
         // Start the loop.
         while ( have_posts() ) : the_post();?>
-    
-        <!-- Titre Natif -->
-        <h1><?php the_title(); ?></h1>
-        <!-- Référence ACF -->
-        <?php echo get_post_meta( get_the_ID(), 'reference', true ); ?>
-        <!-- Catégorie Natif -->
-        <?php the_category(); ?>
-        <!-- Format CPT UI -->
-        <?php the_terms( get_the_ID() , 'format' ); ?>        
-        <!-- Type ACF -->
-        <?php echo get_post_meta( get_the_ID(), 'type', true ); ?>
-        <!-- Année Natif -->
-        <?php the_time( 'Y' ); ?>
-        <!-- Photo Natif -->
-        <?php the_post_thumbnail(); ?>
 
-        <!-- Navigation -->
-        <?php
+        <div class="content-wrapper">
+
+            <div class="content">
+                <div class="content-info">
+                    <h1 class="title"> <?php the_title(); ?></h1>
+                    <p class="property">Référence : <?php echo get_post_meta( get_the_ID(), 'reference', true ); ?></p>
+                    <p class="property">Catégorie : <?php the_category(); ?></p>
+                    <p class="property">Format : <?php the_terms( get_the_ID() , 'format' ); ?> </p>
+                    <p class="property">Type : <?php echo get_post_meta( get_the_ID(), 'type', true ); ?></p>
+                    <p class="property">Année : <?php the_time( 'Y' ); ?></p>
+                </div>
+
+                <!-- div ou directement image? -->
+                <div class="content-photo">
+                    <?php echo the_post_thumbnail(); ?>
+                </div>
+            </div>
+
+            <div class="interact">
+                <div class="interact-contact">
+                    <p class="text">Cette photo vous intéresse ?</p>
+                    <button class="grey-btn">Contact</button>
+                </div>
+
+                <div class="interact-nav">
+
+                    <?php
         // Replace the image URLs with your actual arrow image URLs
         $previous_arrow_image_url = get_template_directory_uri() . '/assets/images/Line 6.png';
         $next_arrow_image_url = get_template_directory_uri() . '/assets/images/Line 7.png';
@@ -71,25 +42,41 @@ get_header(); ?>
         // Custom format strings for previous and next post links with arrow images
         $previous_link_format = '<img src="' . $previous_arrow_image_url . '" alt="Previous Post" />';
         $next_link_format = '<img src="' . $next_arrow_image_url . '" alt="Next Post" />';
-        
-        // Suivant 
-        next_post_link('%link', $next_link_format);
-        $next_post = get_next_post();
-        echo get_the_post_thumbnail( $next_post, [ 100, 100 ] ); 
-
-        // Précédent
-        previous_post_link('%link', $previous_link_format);
-        $previous_post = get_previous_post();
-        echo get_the_post_thumbnail( $previous_post, [ 100, 100 ] ); 
-
         ?>
+                    <div class="previous-wrapper">
+                        <div class="arrow">
+                            <?php previous_post_link('%link', $previous_link_format); ?>
+                        </div>
+                        <div class="arrow-img">
+                            <?php 
+                            $previous_post = get_previous_post();
+                            echo get_the_post_thumbnail( $previous_post, [ 100, 100 ] ); 
+                            ?>
+                        </div>
+                        <div class="next-wrapper">
+                            <div class="arrow">
+                                <?php next_post_link('%link', $next_link_format); ?>
+                            </div>
+                            <div class="arrow-img">
+                                <?php 
+                                $next_post = get_next_post();
+                                echo get_the_post_thumbnail( $next_post, [ 100, 100 ] ); 
+                                ?>
+                            </div>
+                        </div>
 
-        <?php
+                    </div>
+
+                </div>
+
+            </div>
+
+            <?php
         // End the loop.
         endwhile;
         ?>
- 
-        </main><!-- .site-main -->
-    </div><!-- .content-area -->
- 
+
+    </main><!-- .site-main -->
+</div><!-- .content-area -->
+
 <?php get_footer(); ?>
