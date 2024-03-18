d68ddf65dd2c4355401192a2a02d710212078ea0
exploralgo <admin@exploralgo.fr>
2024-03-18 07:40:52 +0100

    css bloc contenu

--- wp-content/themes/motaphoto/single-photo.php
+++ wp-content/themes/motaphoto/single-photo.php
@@ -9,40 +9,38 @@ get_header(); ?>
         while ( have_posts() ) : the_post();?>
 
         <div class="content-wrapper">
-
             <div class="content">
                 <div class="content-info">
                     <h1 class="title"> <?php the_title(); ?></h1>
                     <p class="property">Référence : <?php echo get_post_meta( get_the_ID(), 'reference', true ); ?></p>
-                    <p class="property">Catégorie : <?php the_category(); ?></p>
-                    <p class="property">Format : <?php the_terms( get_the_ID() , 'format' ); ?> </p>
+                    <p class="property">Catégorie : <?php echo get_the_category()[0]->cat_name; ?></p>
+                    <p class="property">Format : <?php echo get_the_terms( get_the_ID() , 'format' )[0]->name; ?> </p>
                     <p class="property">Type : <?php echo get_post_meta( get_the_ID(), 'type', true ); ?></p>
                     <p class="property">Année : <?php the_time( 'Y' ); ?></p>
-                </div>
 
+                </div>
                 <!-- div ou directement image? -->
                 <div class="content-photo">
                     <?php echo the_post_thumbnail(); ?>
                 </div>
             </div>
-
             <div class="interact">
                 <div class="interact-contact">
                     <p class="text">Cette photo vous intéresse ?</p>
                     <button class="grey-btn">Contact</button>
                 </div>
-
                 <div class="interact-nav">
 
                     <?php
-        // Replace the image URLs with your actual arrow image URLs
-        $previous_arrow_image_url = get_template_directory_uri() . '/assets/images/Line 6.png';
-        $next_arrow_image_url = get_template_directory_uri() . '/assets/images/Line 7.png';
+                    // Replace the image URLs with your actual arrow image URLs
+                    $previous_arrow_image_url = get_template_directory_uri() . '/assets/images/Line 6.png';
+                    $next_arrow_image_url = get_template_directory_uri() . '/assets/images/Line 7.png';
 
-        // Custom format strings for previous and next post links with arrow images
-        $previous_link_format = '<img src="' . $previous_arrow_image_url . '" alt="Previous Post" />';
-        $next_link_format = '<img src="' . $next_arrow_image_url . '" alt="Next Post" />';
-        ?>
+                    // Custom format strings for previous and next post links with arrow images
+                    $previous_link_format = '<img src="' . $previous_arrow_image_url . '" alt="Previous Post" />';
+                    $next_link_format = '<img src="' . $next_arrow_image_url . '" alt="Next Post" />';
+                    ?>
+                    
                     <div class="previous-wrapper">
                         <div class="arrow">
                             <?php previous_post_link('%link', $previous_link_format); ?>
