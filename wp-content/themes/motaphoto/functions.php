02f377433b9d9cd6a0ac7784f542b08842c00d3f
exploralgo <admin@exploralgo.fr>
2024-04-13 14:40:35 +0200

    tri fichiers ok

--- wp-content/themes/motaphoto/functions.php
+++ wp-content/themes/motaphoto/functions.php
@@ -1,5 +1,9 @@
 <?php
 
+foreach(glob(get_template_directory() . "/inc/*.php") as $file){
+  require $file;
+}
+
  add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
  function theme_enqueue_scripts() {
     // chargement css
@@ -20,10 +24,6 @@
     wp_register_style( 'select2', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css');
     wp_enqueue_style('select2');
 
-    // Fontawesome
-    wp_register_script( 'fontawesome', 'https://kit.fontawesome.com/c03f12b2fd.js');
-    wp_enqueue_script('fontawesome');
-
     $translation_array = array( 'templateUrl' => get_template_directory_uri() );
     //after wp_enqueue_script
     wp_localize_script( 'custom-script', 'object_name', $translation_array );
@@ -40,9 +40,8 @@ add_theme_support( 'title-tag' );
 // encodning apostrophe
 add_filter('run_wptexturize', '__return_false');
 
-
 // gestion des images à la une 
-// add_theme_support( 'post-thumbnails' );
+add_theme_support( 'post-thumbnails' );
 
 // Emplacement des menus
 add_action( 'init', 'register_my_menus' );
@@ -66,116 +65,4 @@ function register_my_menus() {
 }
 add_filter( 'wp_image_editors', 'wpb_image_editor_default_to_gd' );
 
-// V2 AJAX
 
-add_action( 'wp_ajax_recuperer_custom_posts', 'recuperer_custom_posts' );
-add_action( 'wp_ajax_nopriv_recuperer_custom_posts', 'recuperer_custom_posts' );
-
-function recuperer_custom_posts() {
-	// Vérification de sécurité 
-  // Définir les arguments par défaut
-
-  // Afficher le bon nombre de photos sur les pages (2 pages | !=)
-  // Définie dans le JS
-  $posts_per_page = $_POST['posts']; 
-
-  $args = array(
-      'post_type' => 'photo',
-      'posts_per_page' => $posts_per_page, 
-      'paged' => $_POST['paged'],
-  );
-
-  // Exclure la photo affichée (single page)
-  $post_id = $_POST['postid'];
-
-  if (!empty($post_id)) {
-    $args["post__not_in"] = array($post_id);
-  }
-
-  // Filtres categorie
-    // définie par le filtre select de la page d'acceuil
-  $catSlug = $_POST['category'];
-    // définie par la variable du single post
-  $category_name = sanitize_text_field( $_POST['category'] );
-
-  if (!empty($catSlug) | !empty($category_name)) {
-    $args['tax_query'][] = array(
-        'taxonomy' => 'category',
-        'field'    => 'slug',
-        'terms'    => $catSlug,
-    );
-  }
-
-
-  // filtre format
-    // définie par le filtre select de la page d'acceuil
-  $formatSlug = $_POST['format'];
-    // définie par la variable du single post : pas nécessaire
-
-  if (!empty($formatSlug)) {
-    $args['tax_query'][] = array(
-        'taxonomy' => 'format',
-        'field'    => 'slug',
-        'terms'    => $formatSlug,
-    );
-  }
-  
-  // filtre tri 
-    // définie par le filtre select de la page d'acceuil
-  $order = $_POST['order'];
-    // définie par la variable du single post : pas nécessaire
-  
-  if (!empty($order)) {
-    if ($order == 'asc') {
-      $args["order"] = 'ASC';
-    }
-    if ($order == 'desc') {
-      $args["order"] = 'DESC';
-    }
-  }
-
-  // Query Go
-  $query = new WP_Query($args);
-
-    // var_dump($query);
-
-  $custom_posts = array();
-  // besoin de l'info pour cacher le bouton charger plus
-  $max_pages = $query->max_num_pages;
-  // $nb_posts = 
-
-  if ($query->have_posts()) {
-    while ($query->have_posts()) {
-      $query->the_post();
-
-      // Récupérer les données pertinentes des custom posts
-      $custom_post_data = array(
-        'image_url' => get_the_post_thumbnail_url(),
-        'post_url' => get_post_permalink(),
-        'caption' => sanitize_text_field(get_the_title()),
-        'reference' => get_post_meta( get_the_ID(), 'reference', true ),
-        'categorie' => get_the_category()[0]->cat_name,
-      );
-
-      array_push($custom_posts,$custom_post_data);
-
-    }
-
-  }
-
-  wp_reset_postdata();
-
-    // Utilisez un tableau pour stocker les données adaptées en fonction de la page actuelle
-  $localized_data = array(
-      'custom_posts' => $custom_posts,
-      'maxPages' => $max_pages,
-  );
-    
-  wp_send_json_success($localized_data);
-
-  wp_die();
-
-   var_dump($custom_posts);
-   var_dump($localized_data);
-
-}
